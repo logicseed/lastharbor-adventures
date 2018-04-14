7552b87304d13b431ae69a76be1270137e186ef4
Marc King <marc@marc.software>
2018-04-13 22:37:33 -0400

    Add game logic library project

---
+++ GameLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Last Harbor Game Logic Library")]
+[assembly: AssemblyDescription("A library containing the game rules and mechanics.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Flamingo Parade")]
+[assembly: AssemblyProduct("Last Harbor: Adventures")]
+[assembly: AssemblyCopyright("Copyright © Marc King 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c983a2a7-4a3e-4b76-a947-13862f8624cc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
