c8e05c89907587e1b018b7cc9d2ed9a251fc24e6
Marc King <marc@marc.software>
2018-04-13 22:26:14 -0400

    Add adventure client project

---
+++ AdventureClient/Program.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2018 Marc King <marc@marc.software> - All Rights Reserved
+//
+// SPDX-License-Identifier: MIT
+//
+// This work is licensed under the terms of the MIT license. See <LICENSE.md> for more information.
+
+namespace FlamingoParade.LastHarbor.Adventures
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
