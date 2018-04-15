d8cc060d435ed6c4a2760c06650eb1b9046be2da
Marc King <marc@marc.software>
2018-04-14 21:11:48 -0400

    Add native history info datatype

---
+++ Console/Native/Datatypes/CONSOLE_HISTORY_INFO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastHarbor.Console.Native.Datatypes
+{
+    /// <summary>
+    /// Contains information about the console history.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct CONSOLE_HISTORY_INFO
+    {
+        /// <summary>
+        /// The size of the structure, in bytes.
+        /// </summary>
+        /// <remarks>
+        /// Set this member to <c>sizeof(CONSOLE_HISTORY_INFO)</c>.
+        /// </remarks>
+        public Int32 Size;
+
+        /// <summary>
+        /// The number of commands kept in each history buffer.
+        /// </summary>
+        public Int32 HistoryBufferSize;
+
+        /// <summary>
+        /// The number of history buffers kept for this console process.
+        /// </summary>
+        public Int32 NumberOfHistoryBuffers;
+
+        /// <summary>
+        /// This parameter can be zero or the following value.
+        /// </summary>
+        /// <remarks>
+        /// HISTORY_NO_DUP_FLAG 0x1 Duplicate entries will not be stored in the history buffer.
+        /// </remarks>
+        public Int32 Flags;
+    }
+}
